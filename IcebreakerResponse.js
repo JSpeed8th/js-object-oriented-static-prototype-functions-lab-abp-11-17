c7189d48ab7ea0575dee3a8237fecec8c206b67e
JSpeed8th <jspeed8th@gmail.com>
2018-01-25 20:18:26 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -6,4 +6,8 @@ IcebreakerResponse.BatchCreate = function (email){
   return email.map(function (question) {
     return new IcebreakerResponse (question)
   })
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 5ede721c442b322579701064e8d1b5ca738d9f4c
